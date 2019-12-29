0ae41e93cddb64d00650deec89b6378e95c1de73
Michele Banfi <michi.banfi01@gmail.com>
2019-12-29 20:22:50 +0100

    Updates

---
+++ precache-manifest.8a7b974e07d953d40ef4c7899eb9aea1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc12cff15f0add3b11b82d1572ee0cd",
+    "url": "/RaccoltaDifferenziata/index.html"
+  },
+  {
+    "revision": "dfad2eb0f6dfe69d6785",
+    "url": "/RaccoltaDifferenziata/static/css/main.fb2714a4.chunk.css"
+  },
+  {
+    "revision": "3335cd02ad3574cf51ee",
+    "url": "/RaccoltaDifferenziata/static/js/2.11df8b44.chunk.js"
+  },
+  {
+    "revision": "dfad2eb0f6dfe69d6785",
+    "url": "/RaccoltaDifferenziata/static/js/main.9f33e2d9.chunk.js"
+  },
+  {
+    "revision": "5572d65b33aeb5bfbb89",
+    "url": "/RaccoltaDifferenziata/static/js/runtime-main.972c437b.js"
+  },
+  {
+    "revision": "1cbdf07e485a10bbe7d9adc14a43b02e",
+    "url": "/RaccoltaDifferenziata/static/media/apple.1cbdf07e.svg"
+  },
+  {
+    "revision": "2eeea23c54a854d21e39c575ffda2f09",
+    "url": "/RaccoltaDifferenziata/static/media/glass-bin.2eeea23c.svg"
+  },
+  {
+    "revision": "5ec757c008bf6e5ab004c3de02db63c8",
+    "url": "/RaccoltaDifferenziata/static/media/organic.5ec757c0.svg"
+  },
+  {
+    "revision": "d8a1bcff1167cd66afca3d5a4de5f6ef",
+    "url": "/RaccoltaDifferenziata/static/media/paper-bin.d8a1bcff.svg"
+  },
+  {
+    "revision": "39bb843ac7e998f1a6ad836f001f16e3",
+    "url": "/RaccoltaDifferenziata/static/media/plastic.39bb843a.svg"
+  },
+  {
+    "revision": "0b523f8f39c1d095ec81bb785fb987cc",
+    "url": "/RaccoltaDifferenziata/static/media/recycle.0b523f8f.svg"
+  },
+  {
+    "revision": "2223f95ab4d6953fe36231390983a2ff",
+    "url": "/RaccoltaDifferenziata/static/media/reduce.2223f95a.svg"
+  },
+  {
+    "revision": "d890098ca51fd51ea86ad46f235e2ecb",
+    "url": "/RaccoltaDifferenziata/static/media/share.d890098c.svg"
+  },
+  {
+    "revision": "877ee3cc97d2490d91b9ab9d47e55e9c",
+    "url": "/RaccoltaDifferenziata/static/media/sprout.877ee3cc.svg"
+  }
+]);
